d6e373190404056297a35139b668fa877fd52e87
yangwonjoon <diddnjswns20@gmail.com>
2024-05-02 00:33:05 +0900

    [fix] change routes method

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import { Intro } from './pages/Intro';
 import { Main } from './pages/Main';
-import { Logo } from './component/Logo';
 import { MainSelect } from './pages/MainSelect'
+import { Layout } from './routes/Layout';
 
 const App = () => {
   return (
-    <div className='w-screen h-screen bg-main relative cursor-default'>
-      <Logo></Logo>
-      <Routes>
+    <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Intro></Intro>} />
+      </Route>
+      <Route element={<Layout />}>
         <Route path="/main" element={<Main></Main>} />
         <Route path="/main/select" element={<MainSelect></MainSelect>} />
-      </Routes>
-    </div>
+      </Route>
+
+    </Routes>
   );
 }
 
