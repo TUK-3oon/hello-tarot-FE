715a20f68a50c227b1bf3bd39bbb8ac1ba94e475
yangwonjoon <diddnjswns20@gmail.com>
2024-05-02 18:03:34 +0900

    [feat] change logo component

--- src/component/Logo.tsx
+++ src/component/Logo.tsx
@@ -5,9 +5,9 @@ export const Logo = () => {
         <div className='w-full h-1/5 overflow-hidden flex justify-center relative'>
             <div className='h-4/5 w-64 rounded-b-full relative '>
                 <img src={logo} alt="Logo" className='w-full h-4/5 -mb-2.5' />
-                <div className="absolute bottom-1/2 left-0 transform -translate-x-1/2 translate-y-1/2 bg-logo w-12 h-12">health</div>
-                <div className="absolute bottom-0 left-0 transform translate-x-1/2 translate-y-1/2 bg-logo w-12 h-12">love</div>
-                <div className="absolute bottom-1/2 right-0 transform translate-x-1/2 translate-y-1/2 bg-logo w-12 h-12">info</div>
+                <div className="absolute bottom-1/2 left-0 transform -translate-x-1/2 translate-y-1/2 text-logo">health</div>
+                <div className="absolute bottom-0  right-1/2 transform translate-x-1/2 translate-y-1/2 text-logo">love</div>
+                <div className="absolute bottom-1/2 right-0 transform translate-x-1/2 translate-y-1/2 text-logo">info</div>
             </div>
         </div>
     );
