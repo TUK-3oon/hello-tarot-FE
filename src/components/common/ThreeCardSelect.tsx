8a49d2a23c5d6ff929ef26072c726e58242e6c22
yangwonjoon <diddnjswns20@gmail.com>
2024-05-09 21:46:28 +0900

    [setting] set proxy server

--- src/components/common/ThreeCardSelect.tsx
+++ src/components/common/ThreeCardSelect.tsx
@@ -2,16 +2,33 @@ import { useNavigate } from "react-router-dom";
 import { IntroSelectProps } from "../../types/types";
 import { useVisible } from "../../hooks/useVisible";
 import { Button } from "../ui/button";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ThreeCardSelect = ({ card, close, isActive }: IntroSelectProps) => {
 
     const navigate = useNavigate();
     const { visibleClass } = useVisible()
+    const [cardImage, setCardImage] = useState("");
+
+    useEffect(() => {
+        const fetchCardImage = async () => {
+            try {
+                const res = await axios.get("/card/front/all");
+                console.log(res.data)
+                // setCardImage(response.data.imageUrl);
+            } catch (error) {
+                console.error("Failed to fetch card image:", error);
+            }
+        };
+
+        fetchCardImage();
+    }, []);
 
     return (
         <div className={`fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center transition-opacity duration-1000 ${visibleClass}`}>
             <div className="w-4/5 h-4/5 flex">
-                <div style={{ backgroundImage: `url(${card})` }} className="w-1/2 h-full bg-cover bg-no-repeat bg-center"></div>
+                <div style={{ backgroundImage: `url(${card})` }} className="w-1/2 h-full bg-cover bg-no-repeat bg-center overflow-fit mr-5"></div>
                 <div className="h-full w-1/2 bg-white p-3 pt-6 relative">
                     <div className="h-5/6 overflow-y-auto p-5">
                         <div className="mb-7">
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-//vite.config.ts 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/card": {  // Use "/card" to match the start of your actual API path
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/card/, '/card') // Optional rewrite
+      }
+    }
+  },
+  //아래거는 왜 안되었는지 확인해보자 이건 get('/api/card/front/all'로 받음')
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:8000",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
   plugins: [react()],
   resolve: {
     alias: {
