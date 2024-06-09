ea0fcf02b9b5f44de8edd9f4adbef6d0ec8c2367
yang <diddnjswns20@gmail.com>
2024-06-09 17:47:42 +0900

    [feat] Changed loop statement to search by key value

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,26 +4,23 @@ interface LogoProps {
   gameType: string;
 }
 
-export const Logo = ({ gameType }: LogoProps) => {
-  const getTitle = (type: string) => {
-    switch (type) {
-      case 'love':
-        return 'Love Tarot';
-      case 'health':
-        return 'Health Tarot';
-      case 'fortune':
-        return 'Fortune Tarot';
-      default:
-        return 'Hello Tarot';
-    }
-  };
+interface ILogoName{
+  [key: string] : string
+}
 
+const logoName : ILogoName = {
+  love : "Love Tarot",
+  health : "Health Tarot",
+  fortune : "Fortune Tarot"
+}
+
+export const Logo = ({ gameType }: LogoProps) => {
   return (
     <div className="w-full h-1/5 overflow-hidden flex flex-col justify-center items-center relative">
       <div className="h-3/5 w-64">
         <img src={logo} alt="Logo" className="w-full h-full border-none" />
       </div>
-      <h1 className="text-logo mt-2 text-2xl">{getTitle(gameType)}</h1>
+      <h1 className="text-logo mt-2 text-2xl">{logoName[gameType]}</h1>
     </div>
   );
 };
