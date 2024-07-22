a7d40eebedb87b3e16c810781ab5f59d8f146cf0
yang <diddnjswns20@gmail.com>
2024-07-22 23:25:00 +0900

    [feat] Switch from main/select to main on refresh

--- src/pages/main/MainSelect.tsx
+++ src/pages/main/MainSelect.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { ThreeCard } from '@/components/game/ThreeCard';
-import { useLocation } from 'react-router-dom';
 
 export const MainSelect = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-  const gameData = location.state.gameData;
-  const randomCards = location.state.randomCards
+  const gameData = location.state?.gameData;
+  const randomCards = location.state?.randomCards;
 
-  return <ThreeCard isMain={true} gameData={gameData} randomCards={randomCards}></ThreeCard>
+  useEffect(() => {
+    const isReload = sessionStorage.getItem('isReload');
+
+    if (isReload) {
+      sessionStorage.removeItem('isReload');
+      navigate('/main');
+    } else {
+      sessionStorage.setItem('isReload', 'true');
+    }
+  }, [navigate]);
+
+  
+  // if (!gameData || !randomCards) {
+  //   return null; 
+  // }
+
+  return <ThreeCard isMain={true} gameData={gameData} randomCards={randomCards} />;
 };
