efa6de90ed534148f71d4490534cf478621c6f4d
yang <diddnjswns20@gmail.com>
2024-07-24 19:15:23 +0900

    change Loading Component text gpt to gemini

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -4,7 +4,7 @@ export const Loading = ()=>{
     return <div>
         <PacmanLoader color='#D4D3B8'/>
         <div>
-            <p className='text-logo mt-5'> GPT가 답변중이에요 </p>
+            <p className='text-logo mt-5'> GEMINI가 답변중이에요 </p>
         </div>
     </div>
 }
